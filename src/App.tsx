2bac24b313113e400fad9babcb6825346d180d22
Riddhima Tiwari <riddhimatiwari143@gmail.com>
2026-02-07 03:07:04 +0530

    Fix Vite build and Github Pages Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Explore from "./pages/Explore";
 import Auth from "./pages/Auth";
@@ -20,20 +20,19 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/plant/:id" element={<PlantDetails />} />
-          <Route path="/my-garden" element={<MyGarden />} />
-          <Route path="/disease-detector" element={<DiseaseDetector />} />
-          <Route path="/soil-tester" element={<SoilTester />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/plant/:id" element={<PlantDetails />} />
+        <Route path="/my-garden" element={<MyGarden />} />
+        <Route path="/disease-detector" element={<DiseaseDetector />} />
+        <Route path="/soil-tester" element={<SoilTester />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
